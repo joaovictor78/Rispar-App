24604ce16664bb2953a02f2328576e17958f5eea
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-06-24 13:37:23 -0400

    fix bug: amount have < 100 or amount > 300.000

--- lib/app/modules/credit_simulation/domain/value_objects/amount_vo.dart
+++ lib/app/modules/credit_simulation/domain/value_objects/amount_vo.dart
@@ -3,7 +3,7 @@ import '../errors/credit_simulation_failure.dart';
 class AmountVO {
   double _amount = 1000;
   AmountVO(double amount) {
-    if (amount < 1000 && amount > 300.000) {
+    if (amount < 1000 || amount > 300000) {
       throw AmountNotAvailable(
           message:
               "O valor de emprestimo deve ser maior igual a R\$1.000 e menor igual a R\$300.000");
