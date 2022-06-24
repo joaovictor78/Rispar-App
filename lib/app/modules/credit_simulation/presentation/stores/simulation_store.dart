bfb746b0581cf22565da27934ffd0bdf79278e47
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-06-24 13:44:40 -0400

    bug fix: added asuka to return toast information error

--- lib/app/modules/credit_simulation/presentation/stores/simulation_store.dart
+++ lib/app/modules/credit_simulation/presentation/stores/simulation_store.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../domain/entities/loan_datails_entity.dart';
 import '../../domain/usecases/simulate_credit_usecase.dart';
 import '../states/simulation_states.dart';
+import 'package:asuka/asuka.dart' as asuka;
 
 class SimulationStore extends ValueNotifier<SimulationState> {
   final SimulateCreditUseCase simulateCreditUseCase;
@@ -10,8 +11,10 @@ class SimulationStore extends ValueNotifier<SimulationState> {
   Future<void> simulate(LoanDetailsEntity loanDetails) async {
     value = LoadingSimulationState();
     final response = await simulateCreditUseCase(loanDetails);
-    await Future.delayed(const Duration(seconds: 2));
-    response.fold((l) {}, (r) {
+    await Future.delayed(const Duration(milliseconds: 500));
+    response.fold((l) {
+      asuka.AsukaSnackbar.alert(l.message!).show();
+    }, (r) {
       value = ResultSimulationState(r);
     });
   }
